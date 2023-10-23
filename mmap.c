70181492aac6399d8cd3865a3c9dc0d290ad7635
PechimuthuMithil <119656326+PechimuthuMithil@users.noreply.github.com>
2023-10-23 11:10:55 +0530

    Add files via upload

---
+++ mmap.c
@@ -0,0 +1,197 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <stdint.h>
+#include <sys/mman.h>
+#include <sys/resource.h>
+#include <math.h>
+
+static size_t page_size;
+
+// align_down - rounds a value down to an alignment
+// @x: the value
+// @a: the alignment (must be power of 2)
+//
+// Returns an aligned value.
+#define align_down(x, a) ((x) & ~((typeof(x))(a) - 1))
+
+#define AS_LIMIT	(1 << 25) // Maximum limit on virtual memory bytes
+#define MAX_SQRTS	(1 << 27) // Maximum limit on sqrt table entries
+#define MAX_FAULTS (1 << 12) // Maximum number of pages we can map before the first need for an unmap.
+// if we reduce the number of MAX_FAULTS, the number of faults encountered increases.
+
+struct node { // Used to make a node of the queue (Linked list application of the queue)
+  uintptr_t page_number;  // Used as value
+  struct node* next;    // Next Pointer
+};
+
+struct node* page_queue_front= NULL; // Rear and front pointers to keep the track of the queue's start and end
+struct node* page_queue_rear= NULL;
+
+int faults_encountered = 0; // Stores the number of page faults we faced.
+static double *sqrts;
+uintptr_t prev_fault_addr;  // Stores the address of a number (prev number actually) whose page is valid. We will evacuate this page once number of faults exceeds MAX_FAULTS 
+// Use this helper function as an oracle for square root values.
+
+
+void push_Back(uintptr_t value) // FIFO(Queue) function to insert nodes/ elements at the end to the queue
+{
+  struct node *temp;
+  temp=(struct node *)malloc(sizeof(struct node));
+  if(temp==NULL)
+  {
+    printf("No Memory available\n");
+    exit(0);
+  }
+  temp->page_number = value;
+  temp->next=NULL;
+  if(page_queue_front == NULL){
+    page_queue_front  = temp;
+    page_queue_rear = temp;
+  }else{
+    page_queue_rear ->next = temp;
+    page_queue_rear = temp;
+  }
+  return;
+}
+
+uintptr_t pop_Front() // Function to remove the the front element from the queue
+{
+  struct node *temp;
+  uintptr_t pg_number;
+
+  if(page_queue_front == NULL && page_queue_rear == NULL)
+  {
+    printf("The queue is empty can not delete Error\n");
+    exit(0);
+  }
+
+  temp = page_queue_front;
+  page_queue_front = page_queue_front->next;
+  pg_number = temp->page_number;
+  
+  free(temp);
+  return pg_number;
+  
+  
+}
+
+static void
+calculate_sqrts(double *sqrt_pos, int start, int nr)
+{
+  int i;
+
+  for (i = 0; i < nr; i++)
+    sqrt_pos[i] = sqrt((double)(start + i));
+}
+
+
+// We have implemented a queue to keep the number of faults in bound and if they go beyond the bound we remove the first element(pop_Front)
+//  we remove the element (first entered page) from the queue and unmap it, and insert when a new page is added.
+
+static void
+handle_sigsegv(int sig, siginfo_t *si, void *ctx)
+{
+  uintptr_t fault_addr = (uintptr_t)si->si_addr;
+  uintptr_t req_page_number = align_down(fault_addr, page_size); // page number of the page in which the req sqrt[pos] is present.
+  faults_encountered += 1;                                       // increasing the counter each time we enter the signal handler
+  if (faults_encountered >= MAX_FAULTS){   
+    uintptr_t prev_page_number= pop_Front();
+    if (munmap((void*)prev_page_number, page_size) == -1) {
+      fprintf(stderr, "Couldn't munmap() the previous page; %s\n",
+              strerror(errno));
+      exit(EXIT_FAILURE);
+    }
+  }
+  
+  double *mapped_page = mmap((void*)req_page_number, page_size, PROT_READ | PROT_WRITE,  // Newly mapped page
+                          MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+
+  if (mapped_page == MAP_FAILED) {
+    fprintf(stderr, "Couldn't mmap() a new page for square root values at 0x%lx; %s\n",
+            req_page_number, strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+  int pos = (req_page_number / sizeof(double)) - ((uintptr_t)sqrts/ sizeof(double)); // Found the position wrt to the alloted memory(according to the setup_sqrt_region funtion), sqrts is pointed to it.
+  int nums_in_page = page_size / sizeof(double);
+  calculate_sqrts(mapped_page, pos, nums_in_page);
+  push_Back(req_page_number);
+  return;
+}
+
+static void
+setup_sqrt_region(void)
+{
+  struct rlimit lim = {AS_LIMIT, AS_LIMIT};
+  struct sigaction act;
+
+  // Only mapping to find a safe location for the table.
+  sqrts = mmap(NULL, MAX_SQRTS * sizeof(double) + AS_LIMIT, PROT_NONE,
+	       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if (sqrts == MAP_FAILED) {
+    fprintf(stderr, "Couldn't mmap() region for sqrt table; %s\n",
+	    strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  // Now release the virtual memory to remain under the rlimit.
+  if (munmap(sqrts, MAX_SQRTS * sizeof(double) + AS_LIMIT) == -1) {
+    fprintf(stderr, "Couldn't munmap() region for sqrt table; %s\n",
+            strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  // Set a soft rlimit on virtual address-space bytes.
+  if (setrlimit(RLIMIT_AS, &lim) == -1) {
+    fprintf(stderr, "Couldn't set rlimit on RLIMIT_AS; %s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  // Register a signal handler to capture SIGSEGV.
+  act.sa_sigaction = handle_sigsegv;
+  act.sa_flags = SA_SIGINFO;
+  sigemptyset(&act.sa_mask);
+  if (sigaction(SIGSEGV, &act, NULL) == -1) {
+    fprintf(stderr, "Couldn't set up SIGSEGV handler;, %s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+}
+
+static void
+test_sqrt_region(void)
+{
+  int i, pos = rand() % (MAX_SQRTS - 1);
+  double correct_sqrt;
+
+  printf("Validating square root table contents...\n");
+  srand(0xDEADBEEF);
+
+  for (i = 0; i < 500000; i++) {
+    if (i % 2 == 0)
+      pos = rand() % (MAX_SQRTS - 1);
+    else
+      pos += 1;
+    calculate_sqrts(&correct_sqrt, pos, 1);
+    if (sqrts[pos] != correct_sqrt) {
+      fprintf(stderr, "Square root is incorrect. Expected %f, got %f.\n",
+              correct_sqrt, sqrts[pos]);
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  printf("All tests passed!\n");
+  printf("Total page faults encountered :%d\n",faults_encountered);
+}
+
+int
+main(int argc, char *argv[])
+{
+  page_size = sysconf(_SC_PAGESIZE);
+  printf("page_size is %ld\n", page_size);
+  setup_sqrt_region();
+  test_sqrt_region();
+  return 0;
+}
